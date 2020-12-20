f2e9464a8bc79eebfa6e74bf93e72abd4f822a0f
saipavan4518 <mvspavan001@gmail.com>
2020-12-20 18:47:12 +0530

    Registration updated

--- routes/userRegister-backup.js
+++ routes/userRegister-backup.js
@@ -1,21 +1,22 @@
 const router = require('express').Router();
 const multer = require('multer');
 const db_pool = require("../db-config");
-const path = require("path");
 
 
+var fileNameObtained = ''
 const storage1 = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, "./uploads/profile_images");
     },
     filename: function(req, file, cb){ 
         //windows doesnt support : element, so we need to replace.
-        cb(null, "img_"+new Date().toISOString().replace(/:/g,'')+"_"+path.extname(file.originalname));
+        fileNameObtained = "img_"+new Date().toISOString().replace(/:/g,'')+"_"+file.originalname;
+        cb(null, fileNameObtained);
     }
 });
 
 const fileFilter = (req, file, cb) =>{
-    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
         cb(null, true);
     }else{
         cb(null, false);
@@ -27,43 +28,33 @@ const upload = multer({
     fileFilter: fileFilter,
     limits: {
         fileSize: 1000*1000*10
+        //10 mb max size
     }
 })
 
 router.route("/").post(upload.single('profilepic'),async (req,res)=>{
-    //console.log(req.file);
     const username = req.body.username;
     const fullname = req.body.fullname;
     const regdno = req.body.regdno;
     const email = req.body.email;
     const password = req.body.password;
 
-    console.log(req)
-    console.log(req.file)
-
-    try{
-
-        var path = "http://localhost:8110/"+req.file.path.replace(/\\/g,'/');
-    }catch(e){
-        console.log("error in getting the file")
-    }
-
-    
+    var path = "http://localhost:8110/uploads/profile_images/"+fileNameObtained;
 
     const query = `insert into user values("${username}","${fullname}","${regdno}","${email}","${password}","${path}")`
     
     db_pool.getConnection(function(error,connection){
         if(error){
-            return res.status(503).send({eid:2,details:"Database servers are down",error:error});
+            return res.status(200).send({eid:2,details:"Database servers are down",error:error});
         }
 
         connection.query(query,function(error, results, fields){
             if(error){
-                return res.status(503).send({eid:3,details:"Invalid Query",error:error});
+                return res.status(200).send({eid:3,details:"Invalid Query",error:error});
             }
             return res.send({
                 eid:0,
-                details:"successfully inserted"
+                details:"Registration Successful"
             });
         });
 
